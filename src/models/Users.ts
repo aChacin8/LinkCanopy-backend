68fc8c292097917cc79ef1d3945c5d1b87ced355
mrMcAko <alechacin08@gmail.com>
2025-07-29 20:14:56 -0600

    feat: Add the handle to the user model

---
+++ src/models/Users.ts
@@ -0,0 +1,37 @@
+import mongoose, { Schema } from "mongoose";
+
+interface IUser {
+    handle: string,
+    name: string,
+    email: string,
+    password: string
+} //La interface funciona como espejo
+
+const userSchema = new Schema ({ 
+    handle: {
+        type: String,
+        required: true,
+        trim: true, //Eliminacion automatica de espacios en blanco
+        lowercase: true,
+        unique: true,
+    },
+    name: {
+        type: String,
+        required: true, 
+        trim: true//Si un usario agrega un nombre con espacios en blanco, elimina estos
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+})
+
+const User = mongoose.model<IUser>('User',userSchema) //El modelo de Usuarios con el generic
+export default User
