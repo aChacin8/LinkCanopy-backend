a98547103428b13706b3a974dccdd1aa9ee12a24
mrMcAko <alechacin08@gmail.com>
2025-08-27 20:31:13 -0600

    fix: Refactor CORS configuration to allow multiple origins

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,7 +1,17 @@
 import { CorsOptions} from 'cors';
 
-const CORS = process.env.CORS_ORIGIN || 'http://localhost:5000';
+const allowedOrigins = [
+  "http://localhost:5173", // frontend dev
+  process.env.CORS_ORIGIN  // frontend producción
+];
 
-export const corsConfig : CorsOptions = {
-    origin: CORS
-}
+export const corsConfig: CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("No permitido por CORS"));
+    }
+  },
+};
+
