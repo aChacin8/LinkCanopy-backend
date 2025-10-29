71edd4900b9bd9b9b2d8b387be2a82fb22795d94
mrMcAko <alechacin08@gmail.com>
2025-10-29 12:46:02 -0600

    feat: Updating server message

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,7 @@ import app from "./server";
 const port = process.env.PORT || 3000
 
 app.listen(port, ()=> {
-    console.log(colors.cyan.bold(`Server is running in: http://localhost:${port}/`));
+    console.log(colors.cyan.bold(`Server is running in: https://linkcanopy-backend.onrender.com`));
+    console.log(colors.cyan.bold(`Documentation aviable in https://linkcanopy-backend.onrender.com/api-docs`));
+
 })
